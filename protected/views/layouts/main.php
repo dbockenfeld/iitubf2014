1b78598ed894248f46147de854daeab4b7608c7a
Dan Bockenfeld <d-bockenfeld@northwestern.edu>
2014-08-13 09:15:18 -0500

    Add Google Analytics

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -6,22 +6,38 @@
         <meta name="language" content="en" />
         <title><?php echo $this->pageTitle; ?></title>
         <?php if ($this->siteDescription) : ?>
-        <meta name="description" content="<?php echo $this->siteDescription; ?>">
-        <?php endif; ?>
-        <?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
-        <?php Yii::app()->clientScript->registerScriptFile(Yii::app()->request->baseUrl . '/js/jquery.defuscate.js'); ?>
-        <script>
-            $(document).ready(function() {
-                $('.obfuscated').defuscate();
-            });
-        </script>
+            <meta name="description" content="<?php echo $this->siteDescription; ?>">
+            <?php endif; ?>
+            <?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
+            <?php Yii::app()->clientScript->registerScriptFile(Yii::app()->request->baseUrl . '/js/jquery.defuscate.js'); ?>
+            <script>
+                $(document).ready(function() {
+                    $('.obfuscated').defuscate();
+                });
+            </script>
+            <script>
+                (function(i, s, o, g, r, a, m) {
+                    i['GoogleAnalyticsObject'] = r;
+                    i[r] = i[r] || function() {
+                        (i[r].q = i[r].q || []).push(arguments)
+                    }, i[r].l = 1 * new Date();
+                    a = s.createElement(o),
+                            m = s.getElementsByTagName(o)[0];
+                    a.async = 1;
+                    a.src = g;
+                    m.parentNode.insertBefore(a, m)
+                })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-        <link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/reset_uni.css"/>
-        <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/font-awesome/css/font-awesome.min.css"/>
-        <link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/universal.css"/>
-        <link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/nav.css"/>
-        <link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/footer.css"/>
-        <link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/favicon.ico" type="image/x-icon" />
+                ga('create', 'UA-5732686-1', 'auto');
+                ga('send', 'pageview');
+
+            </script>
+            <link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/reset_uni.css"/>
+            <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/font-awesome/css/font-awesome.min.css"/>
+            <link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/universal.css"/>
+            <link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/nav.css"/>
+            <link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/footer.css"/>
+            <link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/favicon.ico" type="image/x-icon" />
     </head>
 
     <body>
@@ -57,7 +73,7 @@
             </nav>
         </header>
 
-        <?php echo $content; ?>
+<?php echo $content; ?>
 
         <div class="clear"></div>
 
