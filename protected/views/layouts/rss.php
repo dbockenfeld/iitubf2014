cc819cb6d0ae0e257306d7fd2f731fe1639a32ca
Dan Bockenfeld <d-bockenfeld@northwestern.edu>
2015-04-27 14:14:34 -0500

    Added ATOM information to RSS layout

--- protected/views/layouts/rss.php
+++ protected/views/layouts/rss.php
@@ -1,14 +1,14 @@
-<?php header("Content-type: text/xml"); ?>
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'?>
+<?php header("Content-type: application/xml"); ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
 
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 
     <channel>
 
         <title><?php echo $this->rssTitle; ?></title>
 
         <link><?php echo $this->rssUrl; ?></link>
-
+        <atom:link href="<?php echo $this->rssUrl; ?>" rel="self" type="application/rss+xml" />
         <language>en-us</language>
 
         <copyright>&#x2117; &amp; &#xA9; <?php echo date('Y'); ?> IIT UBF</copyright>
