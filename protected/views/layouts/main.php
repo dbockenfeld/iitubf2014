5f77a4ce72326ce95dfad0c9cb60af58499edc66
Dan Bockenfeld <d-bockenfeld@northwestern.edu>
2016-02-17 10:27:46 -0600

    Remove Google Analytics from development environment.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -15,23 +15,25 @@
                 $('.obfuscated').defuscate();
             });
         </script>
-        <script>
-            (function (i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                        m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        <?php if (substr(Yii::app()->request->hostInfo, strpos(Yii::app()->request->hostInfo, '://') + strlen('://')) != "iitubf.local.com") : ?>
+            <script>
+                (function (i, s, o, g, r, a, m) {
+                    i['GoogleAnalyticsObject'] = r;
+                    i[r] = i[r] || function () {
+                        (i[r].q = i[r].q || []).push(arguments)
+                    }, i[r].l = 1 * new Date();
+                    a = s.createElement(o),
+                            m = s.getElementsByTagName(o)[0];
+                    a.async = 1;
+                    a.src = g;
+                    m.parentNode.insertBefore(a, m)
+                })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-            ga('create', 'UA-5732686-1', 'auto');
-            ga('send', 'pageview');
+                ga('create', 'UA-5732686-1', 'auto');
+                ga('send', 'pageview');
 
-        </script>
+            </script>
+        <?php endif; ?>
         <script>
             $(document).ready(function () {
                 $(".search img").click(function () {
